1b19c6b53b5526bc096931e87ce7ca912dae924d
poornimadevi901 <125128900+poornimadevi901@users.noreply.github.com>
2025-03-05 19:56:47 +0530

    Add files via upload

---
+++ Python basics/Comment.py
@@ -0,0 +1,7 @@
+#This is a single line comment
+
+"""
+This is a multi-line comment.
+It is uesd to write multiple lines as comment.
+"""
+print("Hello")
---
+++ Python basics/Datatypes.py
@@ -0,0 +1,10 @@
+int_var = 25
+print(int_var)
+bool_var = True
+print(bool_var)
+char_var = 'B'
+print(char_var)
+float_var = 97.5
+print(float_var)
+double_var = 76.25683
+print(double_var)
