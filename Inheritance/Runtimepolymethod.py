605e4360e5dafeb04a40fe6ffae1ff9bb7ed5fc0
poornimadevi901 <125128900+poornimadevi901@users.noreply.github.com>
2025-03-06 22:56:36 +0530

    Add files via upload

---
+++ Inheritance/Runtimepolymethod.py
@@ -0,0 +1,21 @@
+class A:
+    def override_method(self):
+        print("Overridden method in Class A")
+class B(A):
+    def override_method(self):
+        print("Overridden method in Class B")
+class C(B):
+    def override_method(self):
+        print("Overridden method in Class C")
+if __name__ == "__main__":
+    refB = A()
+    refB.override_method()  # A's method
+
+    refB = B()
+    refB.override_method()  # B's method
+
+    refC = A()
+    refC.override_method()  # A's method
+
+    refC = C()
+    refC.override_method()  # C's method
