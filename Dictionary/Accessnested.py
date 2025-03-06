5c4142de5c979361ebfe40d6ee32b712247febbb
poornimadevi901 <125128900+poornimadevi901@users.noreply.github.com>
2025-03-07 00:03:17 +0530

    Accessnested.py

---
+++ Dictionary/Accessnested.py
@@ -0,0 +1,7 @@
+students = {
+    101: {"name": "Poori", "age": 21, "grade": "A"},
+    102: {"name": "Teja", "age": 28, "grade": "B"},
+}
+
+print(students[101]["name"])
+print(students[102]["age"])
